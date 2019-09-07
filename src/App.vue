564f5e925f3e7a3a00a2771f2958e87a010154d0
markrobison <markrobison@iprosystems.com>
2019-09-06 21:17:57 -0500

    adding in header to app base

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="app">
+    <headerComponent></headerComponent>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import headerComponent from '@/components/header'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    'headerComponent': headerComponent
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  -moz-osx-font-smoothing: grayscale;*/
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*color: #2c3e50;
+  margin-top: 60px;*/
 }
 </style>
