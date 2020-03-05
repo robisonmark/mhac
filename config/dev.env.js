dc211bc4a6b22ad12e1bf744963cb35b9343395d
markrobison <markrobison@iprosystems.com>
2020-03-05 16:32:42 -0600

    update endpoint

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ENDPOINT: '"https://api.robisonhouse.com/"'
+  // API_ENDPOINT: '"https://api.robisonhouse.com/"'
+  API_ENDPOINT: '"https://api.mhacsports.com/"'
+
 })
