415f21332bb8ad675b802157b15244e3d3327d9b
Mark Robison <ballaboy@gmail.com>
2021-06-19 10:59:23 -0500

    basis of component design

---
+++ src/components/front-pages/blocks/content_center.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="content-center" id="content-block_center">
+    <h2>Page Title</h2>
+    <div class="container">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo veniam similique porro pariatur, at quod, nesciunt optio, aut beatae voluptates voluptatibus voluptas molestiae culpa. Culpa fuga eligendi ex sed sapiente?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo veniam similique porro pariatur, at quod, nesciunt optio, aut beatae voluptates voluptatibus voluptas molestiae culpa. Culpa fuga eligendi ex sed sapiente?
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'content_left',
+  data () {
+    return {
+
+    }
+  },
+  props: [
+    'data'
+  ]
+}
+</script>
+
+<style lang="less">
+  @import "../../../assets/less/elements/typography.less";
+    .content-center {
+        text-align: center;
+        padding: 1rem 1rem 2rem;
+
+        h2 {
+          padding-bottom: 1rem;
+          font-family: @montserrat;
+          font-weight: 400;
+        }
+        .container {
+            width: 85%;
+            text-align: left;
+            margin: auto;
+            max-width: 1260px;
+            line-height: 1.3;
+        }
+    }
+</style>
---
+++ src/components/front-pages/blocks/content_right-image_left.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="content-right">
+      <!-- <div class="content-right_image" :background-image="data.background_img"></div> -->
+      <div class="content-right_image-container">
+        <div class="content-right_image"></div>
+      </div>
+      <div class="content-right_container v-center">
+        <div class="content-right_content">
+          <h3>2021 - 18u Girls</h3>
+          <h4>Chattanooga Patriots</h4>
+          <p><span class="weight-light">Record (W-L-T):</span> 15-1-1</p>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'content_right',
+  data () {
+    return {
+
+    }
+  },
+  props: [
+    'data'
+  ]
+
+}
+</script>
+
+<style lang="less">
+  @import "../../../assets/less/elements/typography.less";
+  .content-right {
+    display: flex;
+    min-height: 200px;
+
+    &_image {
+      order: 1;
+      background: url('../../../assets/img/MHAC_2-optimized.png');
+      background-size: cover;
+      background-position: center;
+      width: 125%;
+      height: 100%;
+    }
+
+    &_container {
+      // one option is an svg background that creates what I am looking for to scale
+      order: 2;
+      background-color: #fff;
+      text-align: right;
+      padding: 1rem;
+      transform-origin: right;
+      position: relative;
+      &:after{
+        content: '';
+        display: block;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        background: #fff;
+        width: 1250px;
+        max-height: 250px;
+        z-index: 0;
+        transform: skew(45deg);
+        transform-origin: bottom;
+        height: 100%;
+      }
+    }
+
+    &_content {
+      padding: 1rem;
+      position: relative;
+      z-index:1;
+      color: #0C4B75;
+      h3 {
+        font-family: @montserrat;
+        font-weight: 400;
+        font-size: 1.5rem;
+        font-kerning: normal;
+        margin-bottom: .75rem;
+        letter-spacing: .5px;
+      }
+      h4 {
+        font-weight: 600;
+        font-size: 1.1rem;
+      }
+      p{
+        font-size: 1rem;
+        font-weight: 600;
+        .weight-light {
+          font-weight: 200;
+        }
+      }
+    }
+
+     &_image-container{
+      width: 65%;
+    }
+    &_container {
+      width: 35%;
+    }
+
+    .v-center {
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+    }
+  }
+</style>
