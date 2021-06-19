65fb40c40879932903386e887587f270806602bb
Mark Robison <ballaboy@gmail.com>
2021-06-19 11:00:04 -0500

    creating a page to consume components

---
+++ src/components/front-pages/hallOfFame.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="page-styles">
+      <template v-for="(component, key) in components">
+        <component :is="component" :key="key"></component>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hall_of_fame',
+  data () {
+    return {
+      components: [
+        'content-center',
+        'content-left',
+        'content-right',
+        'content-left',
+        'content-right'
+      ]
+    }
+  },
+  components: {
+    'content-center': () => import('./blocks/content_center.vue'),
+    'content-left': () => import('./blocks/content_left-image_right.vue'),
+    'content-right': () => import('./blocks/content_right-image_left.vue')
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.page-styles {
+ overflow: hidden;
+ background: #fff;
+}
+</style>
